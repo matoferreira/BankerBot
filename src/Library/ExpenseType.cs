762ffcb4df6d2bddcbb69e3076a9aacd197271f8
jandresleal <62621149+jandresleal@users.noreply.github.com>
2021-06-09 21:22:34 -0300

    last changes

--- src/Library/ExpenseType.cs
+++ src/Library/ExpenseType.cs
@@ -5,19 +5,22 @@ namespace Library
 {
     public class ExpenseType
     {
-        public string name;
+        public string Name { get; private set; }
         public List<Expense> expenses { get; private set; }
 
 
-        public ExpenseType (string name ){
+        public ExpenseType (string name )
+        {
 
         }
 
-        public double CalculateTotaByType(List<Expense> expenses){
+        public double CalculateTotaByType(List<Expense> expenses)
+        {
             return 0;
         }
 
-         public void AccumulateExpensesByType(List<Expense> expenses){
+         public void AccumulateExpensesByType(List<Expense> expenses)
+         {
             
         }
 
--- src/Library/IEntryFormat.cs
+++ src/Library/IEntryFormat.cs
@@ -2,7 +2,13 @@ using System;
 
 namespace Library
 {
+    //Entryformat es una interface, lo que buscamos con esto es, por el principio de LSP, poder tener más de un formato de entrada independiente del funcionamiento 
+    //del programa, y poder tener un programa que cumpla con el principio de OCP. La forma en que se cumple OCP es que,
+    //implementando una interface como formato de entrada, podemos agregar nuevas APIs de comunicaciones (abierto a la extensión)
+    //sin tener que modificar el comportamiento del programa (cerrado a la modificación). Ya que el programa va a esperar un objeto
+    //del tipo IEntryFormat pudiendo ser substituido por cualquier subtipo de ésta (LSP).
     public interface IEntryFormat
     {
+        
     }
 }
