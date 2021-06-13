27dbd5d487da5b7d2cae89358cdee897b085ed2a
matoferreira <matifer1@gmail.com>
2021-06-13 19:06:48 -0300

    Changes made to Wallet and WalletStatement

--- src/Library/Wallet.cs
+++ src/Library/Wallet.cs
@@ -2,6 +2,7 @@
 //Wallet es la clase Experta (patrón Expert) en calcular el balance total de la billetera, dado a que es en sí,
 //una lista de SubWallets, por esta razón, es la que conoce los balances de cada una para calcular el balance total.
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Library
@@ -27,15 +28,34 @@ namespace Library
         {
             SubWalletList.Remove(subwallet);
         }
-        public double GetBalanceByCurrency (Currency currency)
+
+        //En este método necesito recorrer los WalletStatment en SubWalletList para saber los Currency de cada uno pero no sé como referenciar directo al
+        //Statement de cada SubWallet en SubWalletList
+        //Para resolver la forma de aplicar este método se uso la siguiente referencia
+        //https://github.com/ucudal/PII_Conceptos_De_POO/blob/master/Capitulos/3_Tipos_Genericos/3_2_Desarrollo.md
+        public IList GetBalanceByCurrency (Currency currency)
         {
-            return 0;
-            //En este método se devuelve el balance por cada moneda
-            //Se puso "0" para evitar el error de compilación por esperar un retorno double
+            IList result = new ArrayList();
+            foreach (WalletStatement Statement in this.SubWalletList)
+            {
+                if (Statement.Currency.Equals(currency))
+                {
+                    result.Add(Statement.GetBalance());
+                }
+            }
+            return result;
         }
-        public override double GetBalance()
+
+        //Para solucionar el error de abajo se me ocurre hacer un supertipo (o interfaz que no tiene implementación) PaymentMethod
+        //Con dos subtipos uno para wallet y el otro para los demas paymenthmethod
+        public override IList GetBalance()
         {
-            return 0;
+            IList result = new ArrayList();
+            foreach (WalletStatement Statement in this.SubWalletList)
+            {
+                result.Add(Statement.GetBalance());
+            }
+            return result;
         }
     }
 }
