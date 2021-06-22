ecd69c3a7447a9b717c74893fbdf4a93d5f53c95
SoniaOlivera <61550104+SoniaOlivera@users.noreply.github.com>
2021-06-22 18:32:32 -0300

    Clases test y expense analisis

--- src/Library/Expense.cs
+++ src/Library/Expense.cs
@@ -5,7 +5,7 @@ namespace Library
 {
     public class Expense : Transactions
     {
-        
+        public ExpenseType expenseType { get; private set; } 
         public string Concept { get; private set; } 
         public Expense (String concept, double ammount, Currency currency, ExpenseType expenseType)
         {
--- src/Library/ExpensesAnalysis.cs
+++ src/Library/ExpensesAnalysis.cs
@@ -5,23 +5,40 @@ namespace Library
 {    public class ExpenseAnalysis
     {
         public string name;
+
+        ExpenseType expenseType;
         public List<Expense> Expenses;
 
         public double TotalByType = 0;
 
         public ExpenseAnalysis (string name , ExpenseType expenseType){
             this.name = name;
+            
         }
 
         public double CalculateTotalByType(List<Expense> expenses, ExpenseType expenseType){
             foreach (Expense expense in expenses){
-                TotalByType+= expense.Ammount;
+                if (typeof(Expense).IsInstanceOfType(expenseType))
+                    TotalByType+= expense.Ammount;
             }
-        }
+
+            return TotalByType;
+        }   
 
          public void AccumulateExpensesByType(List<Expense> expenses){
-            
+            foreach (Expense expense in expenses)
+           {
+               Expenses.Add(expense);
+           }
         }
+
+        
+         /*public void AccumulateExpensesByType(List<Expense> expenses)
+         {  
+
+           
+         } va para analisis
+*/ 
 
         //no me queda claro que es lo que tiene que hacer el expense analisis para definir los metodos
 
