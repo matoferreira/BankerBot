c4272168f8ebc7fb5bb6c055ca68d96b04624838
jandresleal <62621149+jandresleal@users.noreply.github.com>
2021-06-26 20:44:27 -0300

    savings analysis y mas justificaciones

--- src/Library/Analysis/SavingsAnalysis.cs
+++ src/Library/Analysis/SavingsAnalysis.cs
@@ -1,15 +1,35 @@
 using System;
 using System.Collections.Generic;
 
-//Esta clase es la encargada de analizar el flujo de ahorro de los usuarios
-
+//Esta clase es la encargada de analizar el ahorro de los usuarios
+//Recibe una lista con paymentMethods y los recorre, tomando el ahorro presente en cada cuenta bancaria y en la billetera
+//Devolviendo un string con el siguiente formato: (PaymentMethod),(Total)#, para su posterior uso por Telegram.
+//Es un Singleton pues no tiene sentido que exista más de un objeto.
+//Cumple con SRP pues su única razon de cambio es cuando es llamada por UserInterface.
+//Cuando esto ocurre, esta clase devuelve un string con los gastos desglosados por cuenta.
 namespace Library
 {
     public class SavingsAnalysis
     {   
-        public void AnalyseSavings(List<IPaymentMethod> savingsAccounts)
+        public string AnalyseSavings(List<PaymentMethod> savingsAccounts)
         {
-
+            string lista = "";
+            double total = 0;
+            foreach (PaymentMethod item in savingsAccounts)
+            {
+                if (typeof(BankAccount).IsInstanceOfType(item))
+                {
+                    total = total + item.GetBalance();
+                    lista = lista + $"{((BankAccount)item).BankName},{item.GetBalance()}#";
+                }
+                if (typeof(Wallet).IsInstanceOfType(item))
+                {
+                    total = total + item.GetBalance();
+                    lista = lista + $"Billetera,{item.GetBalance()}#";
+                }
+            }
+            lista = lista + $"Total,{total}#";
+            return lista;
         }
     }
 }
