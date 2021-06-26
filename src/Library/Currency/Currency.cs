be267740fbb5f5732e504ecf79dbeca08d0a0a75
matoferreira <matifer1@gmail.com>
2021-06-26 19:34:56 -0300

    Currency Test y justificaciones en Currency

--- src/Library/Currency/Currency.cs
+++ src/Library/Currency/Currency.cs
@@ -4,12 +4,15 @@ using System;
 //Porque conoce como hacerlo y conoce que moneda es.
 //Cumple con SRP, ya que solo cambia al actualizar su exchange rate con respecto al dolar
 //Y su responsabilidad es convertir la moneda en un valor equivalente en dólares y de dolares a la moneda específica 
+//Por composición, Currency crea un Exchanger subtipo de la Interfaz IExchanger porque este solo existe asociado a una Currency,
+//como currency es quien lo utiliza y delega parte de su operación al Exchanger, por el patrón Creator es la encargada de crear una nueva instanciarlo.
+//Aplicando el Principio DIP (Principios SOLID), Currency depende de una clase de alto nivel (IExchanger) y no de una clase de bajo nivel.
 namespace Library
 {
     public class Currency : ICurrency
     {
         public string Name { get ; set ; }
-        public double ExchangeRate { get ; set ; }
+        public double ExchangeRate { get ; protected set ; }
         public IExchange Exchanger = new CurrencyExchangeAPI();
 
         public Currency(string name)
