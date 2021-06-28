5a5c6aa997cda707c66a495378f96e4e83ff8ea6
jandresleal <62621149+jandresleal@users.noreply.github.com>
2021-06-27 21:29:58 -0300

    compentarios de patrones en los formatters

--- src/Library/EntryFormat/ConsoleReader.cs
+++ src/Library/EntryFormat/ConsoleReader.cs
@@ -1,5 +1,7 @@
 using System;
-
+//ConsoleReader es una implementación de la interfase IStringImput para interactuar con el usuario a través de la
+//consola. Cumple con DIP pues la comunicación es a través de clases de alto nivel y abstracciones (Interfases).
+//Cumple SRP pues su unica razon de cambio es recibir una linea de la consola. 
 namespace Library
 {
     public class ConsoleReader : IStringImput
--- src/Library/EntryFormat/IStringImput.cs
+++ src/Library/EntryFormat/IStringImput.cs
@@ -1,5 +1,6 @@
 using System;
-
+// IStringImput y IIntImput cumplen con ISP pues se utilizan pequeñas interfases para implemaentar solo el comportamiento
+//que las clases necesitan, en lugar de acumularlo todo en IEntryFormat
 namespace Library
 {
     public interface IStringImput : IEntryFormat
